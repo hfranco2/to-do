f9d59e25888cbb1572fee0786efae6ec8f360cb2
hfranco2 <francohenrique10@gmail.com>
2021-12-27 11:05:17 -0300

    finalizando projeto

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const navDone1 = document.querySelector("#navDone1");
 const navDo2 = document.querySelector("#navDo2");
 const navDone2 = document.querySelector("#navDone2");
 const navDo3 = document.querySelector("#navDo3");
-const navDone4 = document.querySelector("#navDone3");
+const navDone3 = document.querySelector("#navDone3");
 const voltar = document.querySelector("#voltar");
 const dia = document.querySelector(".dataDia");
 const dt = new Date();
@@ -59,6 +59,7 @@ primeiroLoad()
     // dia.innerText = selecionado;
    }
 function load() {
+  //usando a variavel nav posso implementar a funcionalidade de poder criar tarefas para outros meses
   if (nav !== 0) {
     dt.setMonth(new Date().getMonth() + nav);
   }
@@ -73,27 +74,25 @@ function load() {
     month: "numeric",
     day: "numeric",
   });
+  //aqui estou criando a data para criar o calendario dinamico
   const diasAntes = diasSemana.indexOf(stringData.split(", ")[0]);
 
   calendario.innerHTML = "";
-
+// aqui uso um loop com a variavel de dias anteriores ao mes mas os dias do mes para criar um calendario que contenha o numero correto de quadrados
   for (let i = 1; i <= diasAntes + diasNoMes; i++) {
     const quadradoDia = document.createElement("div");
     quadradoDia.classList.add("day");
     const stringDia = `${i - diasAntes}/${month + 1}/${year}`;
     quadradoDia.setAttribute("id", stringDia);
-
     if (i > diasAntes) {
       quadradoDia.innerText = i - diasAntes;
-      
-
       if (i - diasAntes === day && nav === 0) {
-        quadradoDia.id = "currentDay";
-      }
+        quadradoDia.id = "currentDay"; }
 
       
 
       quadradoDia.addEventListener("click", () => selecionaDia(stringDia));
+      //quando seleciono o dia ele chama a função selecionaDia para colocar como base para a criação de array e objetos passando o valor do dia selecionado
     } else {
       quadradoDia.classList.add("antes");
     }
@@ -113,7 +112,7 @@ function selecionaDia(dia) {
       day: "numeric",
     });
   }
-
+  //aqui se eu nao selecionei nenhum dia,  ou seja, a primeira vez que estou acessando o to-do no momento, ele seta o dia selecionado como hoje
   let diaBase = dia;
   let getLocal = localStorage.getItem("Dia");
  
@@ -122,27 +121,28 @@ function selecionaDia(dia) {
   } else {
     listaArr = JSON.parse(getLocal); // transformando json em objeto
   }
-  if (listaArr.find(e => e == diaBase)) {
-  //  console.log("ja tem")
+  if (listaArr.find(e => e == diaBase)) {  
   } else {
     listaArr.push(diaBase);
   }
   localStorage.setItem("Dia", JSON.stringify(listaArr)); // transformando objeto em string
  
   limpandoArrayDiaAtual = []
-  
+    //aqui eu limpo o array que indica o dia atual para sempre ser o dia selecionado, pois uso como base para criar os arrays com os objetos dos evetos
     localStorage.setItem("diaAtual", JSON.stringify(limpandoArrayDiaAtual));
     // console.log(diaBase)
     localStorage.setItem("diaAtual", JSON.stringify(diaBase));
 
   
-  
+  //aqui chamo a função diaSelecionado
   diaSelecionado(diaBase)
-  
+  //chamo a função mostrar tarefas para que se houver eventos registrados no array ele ja mostra eles
   mostraTarefas();
+  //chamo a função que cria um array com todos os dias ja visitados
   diasArr()
   calendarioBox.style.display = "none";
 }
+//aqui estou criando o botão para sair ou entrar no calendario
 dia.onclick = () => {
   calendarioBox.style.display = "flex";
 };
@@ -151,6 +151,7 @@ voltar.onclick = () => {
 };
 //aqui eu pego o valor colocado no input pelo usuario
 input.onkeyup = () => {
+  //aqui coloco condição apra que o botão para incluir a tarefa so fique habilitado se escrevi algo no input
   let dadoBase = input.value;
   //uso o metodo trim para tirar espaços em branco do começo e final do input
   if (dadoBase.trim() != 0) {
@@ -164,22 +165,15 @@ input.onkeyup = () => {
 inputBtn.onclick = () => { 
   let getLocalDay = localStorage.getItem("diaAtual");
   listaArrDay = JSON.parse(getLocalDay); 
- 
-  
-  
-  
- 
   //criar o array do dia que ta selecionado
-  //o valor do input seja colocado no array do dia que ta selecionado
-  
+  //o valor do input seja colocado no array do dia que ta selecionado  
   let dadoBase = input.value;
   let getLocal = localStorage.getItem(listaArrDay);
   if (getLocal == null) {
     listaArr = []; // aqui é criado um array para armazenar o json do input
   } else {
     listaArr = JSON.parse(getLocal); // transformando json em objeto
-  }
-  // console.log(getLocal)
+  }  
   listaArr.push({valor:dadoBase,confirmado:false});
   newObj=
   localStorage.setItem(listaArrDay, JSON.stringify(listaArr)); // transformando objeto em string
@@ -191,27 +185,22 @@ function diasArr() {
   let getLocal = localStorage.getItem("Dia");
   let listaArrDay = JSON.parse(getLocal); 
   let listaArr = listaArrDay.map((e)=>e);
-  // console.log(listaArr.length)
-  
-  // console.log(listaArr.length)
+  //aqui criou um novo array usando o map para nao alterar o array original  
   for(z= 0; z<listaArr.length; z++){
     let getLocal2 = localStorage.getItem(listaArr[z]);
-  let listaArrDay2 = JSON.parse(getLocal2); 
-    // console.log(listaArr[z])
-  
+  let listaArrDay2 = JSON.parse(getLocal2);   
   if(listaArrDay2==undefined|listaArrDay2==[]|listaArrDay2==null){
     localStorage.setItem(listaArr[z], JSON.stringify([]));
   }
-    
-  //  console.log(listaArr[z])
-  }
+    }
 }
 
 
 function mostraTarefas(aaa) { 
+  //aqui é a função que mostra todas as tarefas criadas no dia 
   let getLocalDay = localStorage.getItem("diaAtual");
   listaArrDay = JSON.parse(getLocalDay);
-  let getLocal = localStorage.getItem(listaArrDay);  
+  let getLocal = localStorage.getItem(listaArrDay);  //pego o dia atual e procuro o array que tme seu nome
   if (getLocal == null) {
     listaArr = [];
   } else {
@@ -226,8 +215,10 @@ function mostraTarefas(aaa) {
     deleteAllBtn.classList.remove("active");
   }
   let novoItemLista = "";
-
+//crio as divs que contem os eventos dinamicamente usando  o foreach para cada elemento do array do dia atual
   listaArr.forEach((element, index) => {
+    // se o elemento esta confirmado ele ja cria com a classe que deixa o estilo como confirmado
+    if(element.confirmado==false){
     novoItemLista += `<div id="a${index}" ><input type="text" class="text" readonly id="id${index}" value="${element.valor}"/>
   <span id="con${index}" onclick="confirmarTarefa(${index})" class="confirmar"><i class="far fa-check-circle"></i></span>
 
@@ -236,23 +227,37 @@ function mostraTarefas(aaa) {
  <span id="save${index}" onclick="salvarTarefa(${index})" class="salvar"><i class="fas fa-upload"></i></span>
  <span onclick="deleteTarefa(${index})" id="delete"><i class="fas fa-trash"></i></span>
 </div>`;
-  });
-  lista.innerHTML = novoItemLista; // adicionando os li na ul
+  }else{
+    novoItemLista += `<div id="a${index}" ><input type="text" class="text active" readonly id="id${index}" value="${element.valor}"/>
+  <span id="con${index}" onclick="confirmarTarefa(${index})" class="confirmar"><i class="far fa-check-circle"></i></span>
+
+ <span id="edit${index}" onclick="editTarefa(${index})" class="edit" ><i class="fas fa-edit"></i></span>
+
+ <span id="save${index}" onclick="salvarTarefa(${index})" class="salvar"><i class="fas fa-upload"></i></span>
+ <span onclick="deleteTarefa(${index})" id="delete"><i class="fas fa-trash"></i></span>
+</div>`;
+  }
+
+});
+  lista.innerHTML = novoItemLista; // adicionando os li no form
+  //limpo o input
   input.value = "";
   
 }
 function deleteTarefa(index) {
+  //a função deletar tarefa que pega como parametro o index do array do item clicado
   let getLocalDay = localStorage.getItem("diaAtual");
   listaArrDay = JSON.parse(getLocalDay);
   let getLocal = localStorage.getItem(listaArrDay);
   listaArr = JSON.parse(getLocal);
-  listaArr.splice(index, 1); // deletando o item pelo index
+  listaArr.splice(index, 1); // deletando o item pelo index usando o metodo splice
   localStorage.setItem(listaArrDay, JSON.stringify(listaArr));
   mostraTarefas();
 }
 
   //deletar todos os itens
   deleteAllBtn.onclick = () => {
+    //aqui deleto todas as tarefas do dia selecionado
     let getLocalDay = localStorage.getItem("diaAtual");
     listaArrDay = JSON.parse(getLocalDay);
     listaArr = [];
@@ -260,15 +265,18 @@ function deleteTarefa(index) {
     mostraTarefas();
   };
   function salvarTarefa(index) {
+    //aqui é a função que salva a tarefa quando eu edito o valor do input
     let getLocalDay = localStorage.getItem("diaAtual");
   listaArrDay = JSON.parse(getLocalDay);
     let getLocal = localStorage.getItem(listaArrDay);
     listaArr = JSON.parse(getLocal);
+    //pego o valor do dia atual, procuro o array que tem seu nome e uso o index passado no clic para localizar o item certo   
     let input = document.querySelector(`#id${index}`);
     let dadoAtt = input.value;
-    listaArr[index] = dadoAtt;
+    listaArr[index].valor = dadoAtt;
+    //altero o valor da propriedade valor do objeto no array do dia selecionado
     localStorage.setItem(listaArrDay, JSON.stringify(listaArr));
-
+    //faço as alterações necessarias no estilo dos botoes
     const listaButtonEdit = document.getElementById(`edit${index}`);
     const listaButtonSave = document.getElementById(`save${index}`);
     const listaButtonCon = document.getElementById(`con${index}`);
@@ -282,10 +290,9 @@ function deleteTarefa(index) {
     
   }
   function confirmarTarefa(index) {
+    //função que muda o status da tarefa em questão para confirmado ou não confirmado
     const listaButtonEdit = document.getElementById(`edit${index}`);
-    const listaButtonEdit2 = document.getElementById(`id${index}`);
-    let getLocal = localStorage.getItem("confirmados");
-    let valor = listaButtonEdit2.value+listaButtonEdit2.id
+    const listaButtonEdit2 = document.getElementById(`id${index}`);    
     let getLocalEvent = localStorage.getItem("diaAtual");
     listaArrEvent = JSON.parse(getLocalEvent);
     let getLocalEvent2 = localStorage.getItem(listaArrEvent);
@@ -295,33 +302,30 @@ function deleteTarefa(index) {
     let variavel=listaArr[num]
     
     let input = document.querySelector(`#id${index}`);
+    //aqui eu verifico se a classe ja esta ativa, se não o botão vai ativar, se esta ele vai desativar
     if (input.classList != "text active") {
-       variavel.confirmado=true
-    
+      //mudo o valor da propriedade confirmado
+       variavel.confirmado=true    
       localStorage.setItem(listaArrEvent, JSON.stringify(listaArr));
+      //mudo o estilo do objeto em questão
       input.classList.add("active");
       listaButtonEdit.classList.add("active");
       // console.log(listaArrEvent)
     } else {
       variavel.confirmado=false
-      localStorage.setItem(listaArrEvent, JSON.stringify(listaArr));
-      
+      localStorage.setItem(listaArrEvent, JSON.stringify(listaArr));      
       input.classList.remove("active");
       listaButtonEdit.classList.remove("active");
-      // console.log(listaArr)
+      
     }
   }
   function editTarefa(index) {
-   
+    //aqui eu altero a propriedade do input e seu estilo para poder editar e depois chamar a fuinção salvarTarefa
     const listaInput = document.getElementById(`id${index}`);
     const listaButtonEdit = document.getElementById(`edit${index}`);
     const listaButtonSave = document.getElementById(`save${index}`);
     const listaButtonCon = document.getElementById(`con${index}`);
-
-   
-
     listaInput.removeAttribute("readonly");
-
     listaButtonEdit.classList.add("active");
     listaButtonCon.classList.add("active");
     listaButtonSave.classList.add("active");
@@ -330,9 +334,12 @@ function deleteTarefa(index) {
     
   }
   function mostraTodasTarefas() {    
+    //aqui euy crio a função mostrar todas as tarefas onde ela chama um outro container com seu visual
     container2.style="display:grid"
+    container3.style="display:none"
     container1.style="display:none"    
    navDo2.setAttribute("onclick","(voltarTarefas1())")
+   // crio um variavel onde apenas coloco os valores nela para depois setar com innerHTML
     let tt=""
     let getLocalDay = localStorage.getItem("Dia");
     listaArrDay = JSON.parse(getLocalDay);
@@ -342,42 +349,34 @@ function deleteTarefa(index) {
     listaArrEvent = JSON.parse(getLocalEvent);
     let getLocal = localStorage.getItem(listaArrEvent);
     listaArr = JSON.parse(getLocal);      
-    // let getLocalEvent = listaArrDay3.map((e)=>localStorage.getItem(e) );
-    // console.log(getLocalEvent)   
-  //   listaArrDay3.map((e,index)=> {tt +=`<div id="a2${e}" ><input type="text" class="text2" readonly id="id2${e}" value="${e}"/></div>`
-  //   let getLocalEvent = localStorage.getItem(e); 
-  //   listaArrEvent = JSON.parse(getLocalEvent); 
-  //   let getLocal = localStorage.getItem(listaArrEvent);       
-  //   if(listaArrEvent!=[]|listaArrEvent!=undefined|listaArrEvent!=null){
-  //     console.log(listaArrEvent)
-  // tt += `<div id="a${listaArrEvent}" ><input type="text" class="text" readonly id="id${listaArrEvent}" value="${listaArrEvent}"/>        
-  //    </div>`;}})
+   //pego o array do dia atual para manipulalo
      for (i =0; i<listaArrDay3.length; i++){
       //  console.log(listaArrDay3)
       let getLocal2 = localStorage.getItem(listaArrDay3[i]);
         listaArr2 = JSON.parse(getLocal2);
-       
-        
+        //pego cada array de cada dia para alterar        
         if (listaArr2[i]!=null|listaArr2[i]!=undefined|listaArr2[i]!=[]){
           tt +=`<div id="a2${listaArrDay3[i]}" ><input type="text" class="text2" readonly id="id${listaArrDay3[i]}" value="${listaArrDay3[i]}"/></div>`
-        }
-        //  console.log(listaArr2[i])
-        
+        }      
+        //para cada evento de cada dia eu adiciono na minha variavel tt o innerHTMl
           for (u =0; u<listaArr2.length; u++){
              tt += `<div id="a${listaArr2[u]}" ><input type="text" class="text" readonly id="id${listaArr2[u]}" value="${listaArr2[u].valor}"/>        
           </div>`;
           }     }
    
    
-    lista2.innerHTML = tt; // adicionando os li na ul
+    lista2.innerHTML = tt; // adicionando os li no form
    
   }   
   // mostraTodasTarefas()
  
    function mostraTarefasConcluidas() {       
+     //aqui eu crio a função para mostrar todas as tarefas concluidas
      container3.style="display:grid"
+     container2.style="display:none"
      container1.style="display:none"    
-    navDone3.setAttribute("onclick","(voltarTarefas2())")
+      navDone3.setAttribute("onclick","(voltarTarefas2())")
+    //uso a mesma logica da função mostrar todas as tarefas porem uso um if para verificar se o valor dos eventos de cada dia tem a propriedade confirmado com true ou não
     let tt=""
     let getLocalDay = localStorage.getItem("Dia");
     listaArrDay = JSON.parse(getLocalDay);
@@ -390,28 +389,26 @@ function deleteTarefa(index) {
       let getLocal2 = localStorage.getItem(listaArrDay3[i]);
         listaArr2 = JSON.parse(getLocal2);               
         if (listaArr2[i]!=null|listaArr2[i]!=undefined|listaArr2[i]!=[]){
-          
-          tt +=`<div id="a2${listaArrDay3[i]}" ><input type="text" class="text2" readonly id="id${listaArrDay3[i]}" value="${listaArrDay3[i]}"/></div>`
+                    tt +=`<div id="a2${listaArrDay3[i]}" ><input type="text" class="text2" readonly id="id${listaArrDay3[i]}" value="${listaArrDay3[i]}"/></div>`
         } 
-        // console.log(listaArr2[i])        
           for (u =0; u<listaArr2.length; u++){
-            console.log(listaArr2[u].confirmado)
+            // se ele ta confirmado é criado uma div para ele com o devidop estilo
             if(listaArr2[u].confirmado==true){
-              console.log(listaArr2[u].confirmado)
              tt += `<div id="a${listaArr2[u]}" ><input type="text" class="text3" readonly id="id${listaArr2[u]}" value="${listaArr2[u].valor}"/>        
           </div>`;
           }   }  }   
     lista3.innerHTML = tt; // adicionando os li na ul   
   }   
   function voltarTarefas1(){
+    //função para fechas a visualizaçãso de todas as tarefas
     container2.style="display:none"
     container1.style="display:grid"
-    navDo3.setAttribute('onclick','mostraTarefasConcluidas()')
+    navDo2.setAttribute('onclick','mostraTarefasConcluidas()')
   }
   function voltarTarefas2(){
+     //função para fechas a visualizaçãso de todas as tarefas concluidas
     container3.style="display:none"
-     container1.style="display:grid"
-    
+     container1.style="display:grid"    
      navDone3.setAttribute('onclick','mostraTarefasConcluidas()')
-  }
-  // mostraTarefasConcluidas()
+       }
+  
