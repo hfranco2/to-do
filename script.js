c32c88c8d459e18974a8eff5be69918a3cd793f6
hfranco2 <francohenrique10@gmail.com>
2021-12-27 13:32:26 -0300

    responsividade

--- script.js
+++ script.js
@@ -330,7 +330,7 @@ function deleteTarefa(index) {
     listaButtonCon.classList.add("active");
     listaButtonSave.classList.add("active");
     listaInput.focus();
-    
+    listaInput.setAttribute('inputmode','text')
     listaInput.setAttribute('enterkeyhint','go')
     // listaInput.select();
     
